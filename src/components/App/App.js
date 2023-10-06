59674887630362aef7da560ef81985b343d71eaf
Gektor Gevorgyan <gektor.gevorgyan@gmail.com>
2023-10-06 20:11:16 +0400

    Added fullText functionality for news cards

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,27 +1,43 @@
-import { Container, Col, Row, Table } from "react-bootstrap";
+import { Container, Col, Row, Table, Card } from "react-bootstrap";
+import { useState } from "react";
 import Header from "../Header";
 import NewCard from "../NewCard";
-import UserCard from "../UserCard";
 
 function App() {
+
+  const [fullTextId, setTextId] = useState(null)
+
+  const cardOnClick = (id) => {
+    if (fullTextId === id)
+      setTextId(null)
+    else
+      setTextId(id)
+  }
+
   const newsList = [
     {
       id: 1,
       img: "/images/image1.avif",
       title: "News title 1",
-      text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy",
+      text: "Lorem Ipsum is simply...",
+      fullText: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy",
+      pubDate: "18.08.2012",
     },
     {
       id: 2,
       img: "/images/image2.jpg",
       title: "News title 2",
-      text: "text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
+      text: "text ever since the 1500s...",
+      fullText: "text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      pubDate: "18.10.2011",
     },
     {
       id: 3,
       img: "/images/image3.webp",
       title: "News title 3",
-      text: "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ",
+      text: "It has survived not only...",
+      fullText: "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ",
+      pubDate: "04.08.2009",
     },
   ];
 
@@ -47,12 +63,9 @@ function App() {
       </Container>
       <Container>
         <Row>
-          <Col lg={3} md={4} sm={6} xs={12}>
-            <UserCard userData={userData} />
-          </Col>
           {newsList.map((item) => {
             return (
-              <Col lg={3} md={4} sm={6} xs={12} key={item.id}>
+              <Col lg={3} md={4} sm={6} xs={12} key={item.id} onClick={() => cardOnClick(item.id)}>
                 <NewCard
                   img={item.img}
                   title={item.title}
@@ -63,7 +76,21 @@ function App() {
           })}
         </Row>
       </Container>
-
+          
+      { fullTextId ?
+      <Container>
+        <Card className="bg-dark text-white">
+          <Card.Img src={newsList[newsList.findIndex((item) => item.id === fullTextId)].img} alt="Card image" />
+          <Card.ImgOverlay>
+            <Card.Title>{newsList[newsList.findIndex((item) => item.id === fullTextId)].title}</Card.Title>
+            <Card.Text>{newsList[newsList.findIndex((item) => item.id === fullTextId)].fullText}
+            </Card.Text>
+            <Card.Text>{newsList[newsList.findIndex((item) => item.id === fullTextId)].pubDate}</Card.Text>
+          </Card.ImgOverlay>
+        </Card>
+      </Container>
+      : null
+      }
       <Container>
         <Row style={{ textAlign: "center" }}>
           <h1>Compare plans</h1>
